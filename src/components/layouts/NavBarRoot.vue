99331e6ed89ca97e941ebdadcda81fb9ca3b02a0
Railton98 <railtonleal98@gmail.com>
2019-01-11 16:04:09 -0300

    new components for layout

---
+++ src/components/layouts/NavBarRoot.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <nav :class="cor">
+    <div class="nav-wrapper container">
+      <router-link :to="url || '/'" class="brand-logo center">
+        {{ logo || 'Library Project' }}
+      </router-link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <slot />
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBarRoot',
+  props: ['cor', 'logo', 'url']
+}
+</script>
+
+<style lang="css" scoped>
+</style>

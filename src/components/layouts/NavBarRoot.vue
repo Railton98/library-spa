b86b09a36078d7c32f2131513049192292c21655
Railton98 <railtonleal98@gmail.com>
2019-01-12 02:31:40 -0300

    adjust logo

--- src/components/layouts/NavBarRoot.vue
+++ src/components/layouts/NavBarRoot.vue
@@ -1,8 +1,11 @@
 <template lang="html">
   <nav :class="cor">
     <div class="nav-wrapper container">
-      <router-link :to="url || '/'" class="brand-logo center">
+      <router-link :to="url || '/'" class="brand-logo center hide-on-med-and-down">
         {{ logo || 'Library Project' }}
+      </router-link>
+      <router-link :to="url || '/'" class="brand-logo center hide-on-med-and-up">
+        {{ logo || 'Library' }}
       </router-link>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <slot />
